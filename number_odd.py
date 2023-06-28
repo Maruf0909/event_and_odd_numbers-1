a295d8eb92ed60f40d21d4382648cca6978bb7dd
Mac <mac@MacBook-Air-Mac.local>
2023-06-28 11:58:53 +0500

    num_odd

--- number_odd.py
+++ number_odd.py
@@ -1,4 +1,15 @@
 #A four-digit integer is given. Find the number of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
+var_int = 7237
+x1 = var_int%10
+var_int = var_int//10
+x2 = var_int%10
+var_int = var_int//10
+x3 = var_int%10
+var_int = var_int//10
+x4 = var_int%10
+var_int = var_int//10
 
 #Print the number of odd digits in the variable "var_int".
+number_of_odd = x1%2+x2%2+x3%2+x4%2
+print(number_of_odd)
